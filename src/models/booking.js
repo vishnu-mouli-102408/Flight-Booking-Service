f6db40e4a3665a41ef1f32b3ece9c820f5a3dc0b
Ganivada Mouli <vishnumouli0@gmail.com>
2023-10-24 21:21:27 +0530

    Created new migration for booking for adding fields in model

--- src/models/booking.js
+++ src/models/booking.js
@@ -27,6 +27,16 @@ module.exports = (sequelize, DataTypes) => {
         values: ["Booked", "InProcess", "Cancelled"],
         defaultValue: "InProcess",
       },
+      noOfSeats: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      totalCost: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
     },
     {
       sequelize,
